45003b400631ab5b17b4d408ea8653f127a67cac
qwe28256 <qwe28256.swu@vip.163.com>
2026-02-14 21:11:40 +0800

    update dep repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,15 +8,16 @@ plugins {
 
 android {
     namespace = "com.example.neumusic"
-    compileSdk = 34 // 或 35
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.neumusic"
         minSdk = 24
-        targetSdk = 34 // 或 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
+        //noinspection WrongGradleMethod
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
         }
@@ -64,7 +65,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
 
     // Compose UI
-    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -91,7 +92,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
